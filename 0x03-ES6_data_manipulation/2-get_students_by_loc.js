8dbe96e1467451923a44e4c89523b614d9ff91eb
eioforkansi <eioforkansi@gmail.com>
2024-10-08 15:00:57 +0100

    Task 3

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,11 @@
+/**
+ * A function getStudentsByLocation that returns
+ * an array of objects who are located in a specific city.
+ * @param {list} students a list of students.
+ * @param {string} city a city.
+ * @returns an array of objects who are located in a specific city.
+ */
+
 export default function getStudentsByLocation(students, city) {
   return students.filter((object) => object.location === city);
 }
