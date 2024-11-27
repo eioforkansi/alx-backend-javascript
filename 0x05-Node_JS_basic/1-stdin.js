d08322386c3be948a01f3a5301fae9e2344eb986
eioforkansi <eioforkansi@gmail.com>
2024-11-27 09:11:54 +0100

    Task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+  console.log(`Your name is: ${input}`);
+  console.log('This important software is now closing');
+  process.exit();
+});
