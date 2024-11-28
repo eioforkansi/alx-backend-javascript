732418b3e89d47845e7722fa8a7fa8826ed7dda4
eioforkansi <eioforkansi@gmail.com>
2024-11-28 16:31:26 +0100

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,20 @@
+/**
+ * Program that displays a message, takes user name
+ * and then ends after displaying a final message
+ */
+
+// Displays the welcome message
 console.log('Welcome to Holberton School, what is your name?');
 
+// Listens to user input on stdin
+process.stdin.setEncoding('utf8');
+
+// Capture the input
 process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  console.log(`Your name is: ${input}`);
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+
+  // End the program
   console.log('This important software is now closing');
   process.exit();
 });
